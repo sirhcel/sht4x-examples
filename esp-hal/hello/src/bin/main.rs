8761db9dee87f98fc80b6a9ff24a5f39fa651a5e
Christian Meusel <christian.meusel@posteo.de>
2025-03-01 22:23:35 +0100

    Perform periodic measurements in esp-hal sync example

--- esp-hal/hello/src/bin/main.rs
+++ esp-hal/hello/src/bin/main.rs
@@ -3,9 +3,11 @@
 
 use defmt::info;
 use esp_hal::clock::CpuClock;
+use esp_hal::delay::Delay;
+use esp_hal::gpio::{DriveMode, Level, Output, Pull};
 use esp_hal::main;
-use esp_hal::time::{Duration, Instant};
 use esp_println as _;
+use sht4x::{Precision, Sht4x};
 
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
@@ -17,12 +19,32 @@ fn main() -> ! {
     // generator version: 0.3.0
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let _peripherals = esp_hal::init(config);
+    let peripherals = esp_hal::init(config);
+
+    info!("Hello world!");
+
+    let i2c_pin_config = esp_hal::gpio::OutputConfig::default()
+        .with_drive_mode(DriveMode::OpenDrain)
+        .with_pull(Pull::Up);
+    let scl = Output::new(peripherals.GPIO18, Level::High, i2c_pin_config);
+    let sda = Output::new(peripherals.GPIO19, Level::High, i2c_pin_config);
+
+    let i2c =
+        esp_hal::i2c::master::I2c::new(peripherals.I2C0, esp_hal::i2c::master::Config::default())
+            .unwrap()
+            .with_scl(scl)
+            .with_sda(sda);
+    let mut sht4x = Sht4x::new(i2c);
+    let mut delay = Delay::new();
+
+    let serial = sht4x.serial_number(&mut delay).unwrap();
+    info!("sensor serial: {:x}", serial);
 
     loop {
-        info!("Hello world!");
-        let delay_start = Instant::now();
-        while delay_start.elapsed() < Duration::from_millis(500) {}
+        let measurement = sht4x.measure(Precision::High, &mut delay).unwrap();
+        info!("sensor measurement: {}", measurement);
+
+        delay.delay_millis(3000);
     }
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-beta.0/examples/src/bin
