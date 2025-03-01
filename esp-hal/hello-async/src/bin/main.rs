43cd5beddd1eeb2ebb74fffcbeed953e69cb5540
Christian Meusel <christian.meusel@posteo.de>
2025-03-01 20:54:31 +0100

    Perform periodic measurements in esp-hal async example

--- esp-hal/hello-async/src/bin/main.rs
+++ esp-hal/hello-async/src/bin/main.rs
@@ -3,10 +3,12 @@
 
 use defmt::info;
 use embassy_executor::Spawner;
-use embassy_time::{Duration, Timer};
+use embassy_time::{Delay, Duration, Timer};
 use esp_hal::clock::CpuClock;
+use esp_hal::gpio::{DriveMode, Level, Output, OutputConfig, Pull};
 use esp_hal::timer::systimer::SystemTimer;
 use esp_println as _;
+use sht4x::{Precision, Sht4xAsync};
 
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
@@ -14,7 +16,7 @@ fn panic(_: &core::panic::PanicInfo) -> ! {
 }
 
 #[esp_hal_embassy::main]
-async fn main(spawner: Spawner) {
+async fn main(_spawner: Spawner) {
     // generator version: 0.3.0
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
@@ -25,13 +27,30 @@ async fn main(spawner: Spawner) {
 
     info!("Embassy initialized!");
 
-    // TODO: Spawn some tasks
-    let _ = spawner;
+    let i2c_pin_config = OutputConfig::default()
+        .with_drive_mode(DriveMode::OpenDrain)
+        .with_pull(Pull::Up);
+    let scl = Output::new(peripherals.GPIO18, Level::High, i2c_pin_config);
+    let sda = Output::new(peripherals.GPIO19, Level::High, i2c_pin_config);
+
+    let i2c =
+        esp_hal::i2c::master::I2c::new(peripherals.I2C0, esp_hal::i2c::master::Config::default())
+            .unwrap()
+            .with_scl(scl)
+            .with_sda(sda)
+            .into_async();
+    let mut sht4x = Sht4xAsync::new(i2c);
+    let mut delay = Delay;
+
+    info!("Hello world!");
+
+    let serial = sht4x.serial_number(&mut delay).await.unwrap();
+    info!("sensor serial: {:x}", serial);
 
     loop {
-        info!("Hello world!");
-        Timer::after(Duration::from_secs(1)).await;
-    }
+        let measurement = sht4x.measure(Precision::High, &mut delay).await.unwrap();
+        info!("sensor measurement: {}", measurement);
 
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-beta.0/examples/src/bin
+        Timer::after(Duration::from_secs(3)).await;
+    }
 }
